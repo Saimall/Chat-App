ba310e21a3c4ccbddf8cf54cf1e3421e5f19cd8b
Saimall <rameshwaramsaimallik@gmail.com>
2024-01-28 16:22:18 +0530

    added dummy data and basic routes using express followed by added nodemon package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express();
+
+app.get("/", (request, response) => {
+  response.send("API end point");
+});
+
+app.get("/api/chat", (request, response) => {
+  response.send(chats);
+});
+
+app.get("/api/chat/:id", (request, response) => {
+  const singlechat = chats.find((c) => c._id === request.params.id);
+  response.send(singlechat);
+});
 
 app.listen(4000, console.log("server started at 4000"));
